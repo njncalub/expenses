3299f6c8fdacaa4fee425730a70f2db50886ce23
Nap Joseph Calub <njncalub@gmail.com>
2018-06-15 13:14:54 +0800

    Added flex-layout.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 // This is currently a lazy implementation, don't use it for production.
 // You should always import material modules separately.
 import { MwsMaterialModule } from './mws-material/mws-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MwsAppComponent } from './app.component';
 import { MwsAccountMenuComponent } from './mws-account-menu/mws-account-menu.component';
 import { MwsNavbarComponent } from './mws-navbar/mws-navbar.component';
+import { MwsSearchbarComponent } from './mws-searchbar/mws-searchbar.component';
 import { MwsDashboardComponent } from './mws-dashboard/mws-dashboard.component';
 
 @NgModule({
@@ -19,12 +21,15 @@ import { MwsDashboardComponent } from './mws-dashboard/mws-dashboard.component';
     BrowserAnimationsModule,
     LayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     MwsMaterialModule,
+    FlexLayoutModule,
   ],
   declarations: [
     MwsAppComponent,
     MwsNavbarComponent,
     MwsDashboardComponent,
+    MwsSearchbarComponent,
     MwsAccountMenuComponent,
   ],
   bootstrap: [
